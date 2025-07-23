454f403d9223d226fa2ba76ab2b73f5c28ad13b3
lcsgborges <lcsgborges@gmail.com>
2025-07-23 17:38:06 -0300

    arrumando testes para exercicios da aula 04

--- curso_fastapi/tests/test_models.py
+++ curso_fastapi/tests/test_models.py
@@ -1,14 +1,23 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from curso_fastapi.models import User
 
 
-def test_create_user_database(session):
-    new_user = User(username='test', password='password', email='test@test.com')
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='test', password='secret', email='teste@test')
+        session.add(new_user)
+        session.commit()
 
-    session.add(new_user)
-    session.commit()
+        user = session.scalar(select(User).where(User.username == 'test'))
 
-    user = session.scalar(select(User).where(User.username == 'test'))
-
-    assert user.id == 1
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'password': 'secret',
+        'email': 'teste@test',
+        'created_at': time,
+        'updated_at': time,
+    }
