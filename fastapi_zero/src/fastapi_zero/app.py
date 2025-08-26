3c04460cfc01421352329a69333fc9439d0f0b1f
lcsgborges <lcsgborges@gmail.com>
2025-08-26 19:42:56 -0300

    adicionando schemas e http status

--- fastapi_zero/src/fastapi_zero/app.py
+++ fastapi_zero/src/fastapi_zero/app.py
@@ -1,8 +1,10 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from fastapi_zero.schemas import Message
 
-app = FastAPI()
+app = FastAPI(title="My API", description="My First API with FastAPI")
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World!'}
