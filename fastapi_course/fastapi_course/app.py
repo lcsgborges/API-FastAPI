ee8ce0d86f8eedf2e8f86b2ca4363319e35c2c52
lcsgborges <lcsgborges@gmail.com>
2025-10-21 19:04:02 -0300

    Fundamentos do desenvolvimento Web

--- fastapi_course/fastapi_course/app.py
+++ fastapi_course/fastapi_course/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-app = FastAPI()
+from .schemas import Message
+
+app = FastAPI(title='Curso FastAPI')
 
 
-@app.get('/')
-def read_root():
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,
+    response_class=JSONResponse,
+    response_model=Message,
+)
+async def root():
     return {'message': 'Hello World'}
